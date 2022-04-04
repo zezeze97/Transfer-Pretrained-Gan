94dacfa09d8036e10e3f43983977f7b774afd126
PaulTHong <1464003194@qq.com>
2022-04-04 11:25:09 +0800

    run.sh

--- run.sh
+++ run.sh
@@ -1,8 +1,9 @@
 # !/bin/bash
-srun --job-name=ht -p GPU36 --nodes 1 --gres=gpu:1 --qos low --time 120:00:00 \
+srun --job-name=ht -p GPU36 --nodes 1 --gres=gpu:1 --cpus-per-task 6 --qos low --time 120:00:00 \
     python -u train.py configs/finetune/finetune_lsun_kitchen_baseline.yaml
 
-
+#srun --job-name=ht -p GPU36 --nodes 1 --gres=gpu:1 --cpus-per-task 6 --qos low --time 120:00:00 \
+    #python -u train.py configs/finetune/finetune_lsun_kitchen_shift_vec2img_batch_mode.yaml
 
 
 
