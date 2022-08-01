9f07e708f7cec9f19010f0c384b30d2661c9aabd
zezeze97 <18428398691@163.com>
2022-08-01 16:47:51 +0800

    add config

--- latentvecs_modeling.py
+++ latentvecs_modeling.py
@@ -76,6 +76,6 @@ def main(method, prefix):
 
 
 if __name__  == '__main__':
-    prefix = 'output/vec2img/flowers_sub1000_cls4_256dim_special_init'
+    prefix = 'output/vec2img/flowers25_cls5_256dim_special_init'
     method = 'gauss mixture'
     main(method, prefix)
