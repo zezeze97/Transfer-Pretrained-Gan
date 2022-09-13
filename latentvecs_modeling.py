71cdb0a9a9abc40e77afe7fa5608d21cabba09b8
zezeze97 <18428398691@163.com>
2022-09-13 09:48:23 +0800

    add config

--- latentvecs_modeling.py
+++ latentvecs_modeling.py
@@ -76,6 +76,6 @@ def main(method, prefix):
 
 
 if __name__  == '__main__':
-    prefix = 'output/vec2img/cathedral_sub1000_cls5_256dim_special_init_fix'
+    prefix = 'output/vec2img/cityscapes_cls5_256dim_special_init_fix'
     method = 'gauss mixture'
     main(method, prefix)
