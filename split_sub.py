0a3994864b797050bc6de7a15fb82a4cce611fcd
zezeze97 <18428398691@163.com>
2022-09-28 16:56:15 +0800

    update

--- split_sub.py
+++ split_sub.py
@@ -2,11 +2,14 @@ import os
 import shutil
 import random
 
-num_of_sample = 25
-image_root_path = 'data/cathedral/0'
-dst_root_path = 'data/cathedral_25_sub/0'
+num_of_sample = 1000
+image_root_path = 'data/cityscapes/0'
+dst_root_path = 'data/cityscapes_sub1000/0'
 all_image_list = os.listdir(image_root_path)
 sample_image_list = random.sample(all_image_list, num_of_sample)
+
+if not os.path.exists(dst_root_path):
+    os.makedirs(dst_root_path)
 
 for img_name in sample_image_list:
     shutil.copy(os.path.join(image_root_path, img_name), os.path.join(dst_root_path, img_name))
