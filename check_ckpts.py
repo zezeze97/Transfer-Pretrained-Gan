fa27564610d4a8fa7188fd71447e25e17ccc1a24
zezeze97 <18428398691@163.com>
2022-10-20 18:23:55 +0800

    interpolatea

---
+++ check_ckpts.py
@@ -0,0 +1,12 @@
+from tabnanny import verbose
+import torch
+
+pretraned_ckpt_path = 'pretrained_ckpt/imagenet/imagenet-8c505f47.pt'
+vec2img_generator_path = 'output/vec2img/cityscapes_256dim_special_init/chkpts/generator.pth'
+device = torch.device("cpu")
+
+pretrained_generator = torch.load(pretraned_ckpt_path, map_location=device)['generator']
+vec2img_generator = torch.load(vec2img_generator_path, map_location=device)
+
+for key in vec2img_generator.keys():
+    print(key, (pretrained_generator['module.'+key]==vec2img_generator[key]).all())
--- interpolate.py
+++ interpolate.py
@@ -75,8 +75,51 @@ else:
 
 # Distributions
 ydist = get_ydist(nlabels, device=device)
-zdist = get_zdist(config['z_dist']['type'], config['z_dist']['dim'],
-                  device=device)
+if config['z_dist']['type'] == 'gauss':
+    zdist = get_zdist(dist_name=config['z_dist']['type'],dim=config['z_dist']['dim'], device=device)
+elif config['z_dist']['type'] == 'multivariate_normal':
+    mean_path = config['z_dist']['mean_path']
+    cov_path = config['z_dist']['cov_path']
+    mean = torch.FloatTensor(np.load(mean_path))
+    cov = torch.FloatTensor(np.load(cov_path))
+    zdist = get_zdist(dist_name=config['z_dist']['type'], dim=config['z_dist']['dim'], mean=mean, cov=cov, device=device)
+elif config['z_dist']['type'] == 'gmm':
+    gmm_components_weight = np.load(config['z_dist']['gmm_components_weight'])
+    gmm_mean = np.load(config['z_dist']['gmm_mean'])
+    gmm_cov = np.load(config['z_dist']['gmm_cov'])
+    zdist = get_zdist(dist_name=config['z_dist']['type'], 
+                        dim=config['z_dist']['dim'], 
+                        gmm_components_weight=gmm_components_weight, 
+                        gmm_mean=gmm_mean, 
+                        gmm_cov=gmm_cov, 
+                        device=device)
+elif config['z_dist']['type'] == 'kde':
+    # load latent vectors npy file
+    latentvec_dir = config['z_dist']['latentvec_dir']
+    for i,filename in enumerate(os.listdir(latentvec_dir)):
+        if i == 0:
+            latentvecs = np.load(os.path.join(latentvec_dir, filename))
+        else:
+            current_vecs = np.load(os.path.join(latentvec_dir, filename))
+            latentvecs = np.concatenate((current_vecs,latentvecs),axis=0)
+
+    print('latentvecs shape: ', latentvecs.shape)
+    zdist = get_zdist(dist_name='kde', dim=config['z_dist']['dim'], latentvecs=latentvecs, device=device)
+
+elif config['z_dist']['type'] == 'gmm2gauss':
+    gmm_components_weight = np.load(config['z_dist']['gmm_components_weight'])
+    gmm_mean = np.load(config['z_dist']['gmm_mean'])
+    gmm_cov = np.load(config['z_dist']['gmm_cov'])
+    zdist = get_zdist(dist_name=config['z_dist']['type'], 
+                        dim=config['z_dist']['dim'], 
+                        gmm_components_weight=gmm_components_weight, 
+                        gmm_mean=gmm_mean, 
+                        gmm_cov=gmm_cov, 
+                        device=device)
+
+else:
+    raise NotImplementedError
+print('noise type: ', config['z_dist']['type'])
 
 
 # Load checkpoint if existant
@@ -86,8 +129,8 @@ epoch_idx = load_dict.get('epoch_idx', -1)
 
 # Interpolations
 print('Creating interplations...')
-nsteps = config['interpolations']['nzs']
-nsubsteps = config['interpolations']['nsubsteps']
+nsteps = 3
+nsubsteps = 10
 
 y = ydist.sample((sample_size,))
 zs = [zdist.sample((sample_size,)) for i in range(nsteps)]
@@ -96,7 +139,8 @@ ts = np.linspace(0, 1, nsubsteps)
 it = 0
 for z1, z2 in zip(zs, zs[1:] + [zs[0]]):
     for t in ts:
-        z = interpolate_sphere(z1, z2, float(t))
+        # z = interpolate_sphere(z1, z2, float(t))
+        z = t * z1 + (1-t) * z2
         with torch.no_grad():
             x = generator_test(z, y)
             utils.save_images(x, path.join(interp_dir, '%04d.png' % it),
