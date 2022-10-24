cf992a62f11b3ae1fe177976cddf1419b72b4f88
zezeze97 <18428398691@163.com>
2022-10-24 15:18:53 +0800

    add config

--- split_sub.py
+++ split_sub.py
@@ -2,9 +2,9 @@ import os
 import shutil
 import random
 
-num_of_sample = 1000
+num_of_sample = 25
 image_root_path = 'data/Oxford-IIIT-Pet/0'
-dst_root_path = 'data/Pet-Sub-1000/0'
+dst_root_path = 'data/Pet-Sub-25/0'
 all_image_list = os.listdir(image_root_path)
 sample_image_list = random.sample(all_image_list, num_of_sample)
 
